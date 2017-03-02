ee539524f42ebfd2abf58ac18f53557843dc3eb3
Eva Lai <evalai.dc@gmail.com>
2017-03-01 23:11:43 -0500

    fixed delete function if other users are attending

--- app/models/gathering.rb
+++ app/models/gathering.rb
@@ -1,7 +1,7 @@
 class Gathering < ApplicationRecord
   belongs_to :language
   has_many :attendances
-  has_many :users, through: :attendances
+  has_many :users, through: :attendances, dependent: :destroy
   has_many :comments, dependent: :destroy
   belongs_to :user
 
